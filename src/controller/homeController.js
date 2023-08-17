d0e0aa89b420e94b1f5d3a947907cafedca10e5f
hoang <nnhoanghd2004@gmail.com>
2023-08-17 22:27:28 +0700

    #5.2

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -3,12 +3,12 @@ import userService from '../service/userService'
 const handlerHomePage = (req, res) => {
     return res.render("home.ejs");
 }
-const handlerUserPage = (req, res) => {
-    return res.render("user.ejs");
+const handlerUserPage = async (req, res) => {
+    const users = await userService.getUserList()
+    return res.render("user.ejs", { users });
 }
 const handleCreateUser = (req, res) => {
     userService.createNewUser(req.body.email, req.body.password, req.body.username)
-    userService.getUserList()
     return res.send("Create User");
 }
 
--- src/service/userService.js
+++ src/service/userService.js
@@ -6,15 +6,15 @@ const hashPassword = (password) => {
 }
 
 
-// create the connection to database
-import mysql from 'mysql2'
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'jwt'
-});
 
-const createNewUser = (email, password, username) => {
+
+
+const bluebird = require('bluebird');
+import mysql from 'mysql2/promise'
+
+const createNewUser = async (email, password, username) => {
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
+
     connection.query(
         'INSERT INTO users (email, password, username) VALUE (?, ?, ?)',
         [email, hashPassword(password), username],
@@ -27,16 +27,24 @@ const createNewUser = (email, password, username) => {
     )
 }
 
-const getUserList = (email, password, username) => {
-    connection.query(
-        'Select * from users',
-        function (err, results, fields) {
-            if (err) {
-                console.log(err);
-            }
-            console.log(results); // results contains rows returned by server
-        }
-    )
+const getUserList = async (email, password, username) => {
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
+    // connection.query(
+    //     'Select * from users',
+    //     function (err, results, fields) {
+    //         if (err) {
+    //             console.log(err);
+    //         }
+    //         console.log(results); // results contains rows returned by server
+    //     }
+    // )
+    try {
+        const [rows, fields] = await connection.execute('Select * from users');
+        return rows;
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 module.exports = {
