22b30b75e1fbc6f3447ccd93bc974ae6ed209035
hoang <nnhoanghd2004@gmail.com>
2023-08-17 19:26:01 +0700

    #5.1 test hash

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,11 +1,14 @@
-// const mysql = require('mysql2');
-
-// // create the connection to database
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'jwt'
-// });
+import mysql from 'mysql2'
+// import bcrypt from 'bcrypt';
+// // hash password
+// const salt = bcrypt.genSaltSync(10);
+// const hash = bcrypt.hashSync(myPlaintextPassword, salt);
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt'
+});
 
 const handlerHomePage = (req, res) => {
     return res.render("home.ejs");
@@ -17,15 +20,15 @@ const handleCreateUser = (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
     let username = req.body.username;
-
     // connection.query(
-    //     'SELECT * FROM users',
+    //     'INSERT INTO users (email, password, username) VALUES (?, ?, ?)', [email, password, username],
     //     function (err, results, fields) {
+    //         if (err) {
+    //             console.log(err);
+    //         }
     //         console.log(results); // results contains rows returned by server
-    //         console.log(fields); // fields contains extra meta data about results, if available
     //     }
     // );
-    console.log(req.body);
     return res.send("Create User");
 }
 
